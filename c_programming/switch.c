855c3763c6513dc63fd4d3bda7751d88f4601e52
Rahul Pathak <rahulzha93@gmail.com>
2019-11-22 21:05:12 +0545

    switch

--- c_programming/switch.c
+++ c_programming/switch.c
@@ -1,3 +1,18 @@
 switch .. case  ==> constants
 
 if .. else if ..else ==> conditions
+
+
+syntax:
+
+    switch(constant)
+    {
+    case const1:
+        //statement
+        break;
+        .
+        .
+        .
+    default:
+        //statement
+    }
